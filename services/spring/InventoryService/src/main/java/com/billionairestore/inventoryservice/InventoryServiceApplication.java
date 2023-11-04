d19cc85833cb23538bb2720cede0ac9e84584280
Wachirapol Klinkasorn <88503947+wachirapol2002@users.noreply.github.com>
2023-11-04 16:42:38 +0700

    InventoryService

---
+++ services/spring/InventoryService/src/main/java/com/billionairestore/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.billionairestore.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
