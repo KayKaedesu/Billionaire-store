0dc255dcfc09814d4c4be1a004f46e8d5a3c3229
Wachirapol Klinkasorn <88503947+wachirapol2002@users.noreply.github.com>
2023-11-04 17:44:17 +0700

    DiscoveryClient

--- services/spring/ProductService/src/main/java/com/billionairestore/productservice/ProductServiceApplication.java
+++ services/spring/ProductService/src/main/java/com/billionairestore/productservice/ProductServiceApplication.java
@@ -3,7 +3,9 @@ package com.billionairestore.productservice;
 import com.billionairestore.productservice.config.RabbitRegisterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
