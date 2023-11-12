1f3e75eff8c5709f02149b3e7d70fc2d57c1a703
Farza03 <jakra.na@gmail.com>
2023-11-12 21:08:43 +0700

    web: move shelf to employee route

--- web/src/router/modules/employee.ts
+++ web/src/router/modules/employee.ts
@@ -9,6 +9,11 @@ const employeeModule: RouteModule = {
       name: 'EmployeeCashier',
       component: () => import('@/views/employee/CashierView.vue'),
     },
+    {
+      path: 'shelf',
+      name: 'EmployeeShelf',
+      component: () => import('@/views/employee/ShelfView.vue'),
+    },
   ],
 }
 
