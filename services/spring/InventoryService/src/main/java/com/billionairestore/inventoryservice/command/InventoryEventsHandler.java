90a8d928d14627eaeec644db0590ff4e0bda47cd
Wachirapol Klinkasorn <88503947+wachirapol2002@users.noreply.github.com>
2023-11-12 16:51:09 +0700

    Update InventoryEventsHandler.java

--- services/spring/InventoryService/src/main/java/com/billionairestore/inventoryservice/command/InventoryEventsHandler.java
+++ services/spring/InventoryService/src/main/java/com/billionairestore/inventoryservice/command/InventoryEventsHandler.java
@@ -1,15 +1,22 @@
 package com.billionairestore.inventoryservice.command;
 
+import com.billionairestore.core.events.BuyProductCreatedEvent;
 import com.billionairestore.core.events.CreatedEvent;
 import com.billionairestore.inventoryservice.core.data.InventoryEntity;
 import com.billionairestore.inventoryservice.core.data.InventoryRepository;
 import com.billionairestore.inventoryservice.core.events.InventoryCreatedEvent;
 import com.billionairestore.inventoryservice.core.events.InventoryDeletedEvent;
+import com.billionairestore.inventoryservice.query.FindInventoryQuery;
+import com.billionairestore.inventoryservice.query.rest.InventoryRestModel;
 import com.billionairestore.productservice.core.data.ProductEntity;
 import com.billionairestore.productservice.core.events.ProductCreatedEvent;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class InventoryEventsHandler {
@@ -35,6 +42,16 @@ public class InventoryEventsHandler {
         inventoryEntity.setQuantity(0);
         inventoryRepository.save(inventoryEntity);
     }
+
+    @EventHandler
+    public void on(BuyProductCreatedEvent event){
+        System.out.println("buy");
+        InventoryEntity inventoryEntity = inventoryRepository.findByProductId(event.getProductId());
+        inventoryEntity.setQuantity(inventoryEntity.getQuantity()+event.getQuantity());
+        System.out.println(inventoryEntity);
+        inventoryRepository.save(inventoryEntity);
+    }
+
 
     @EventHandler
     public void on(InventoryDeletedEvent event){
