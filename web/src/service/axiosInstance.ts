0db64047572092ad830adfab0ddb541ccef24e57
munkeawtoast <munkeawtoast@duck.com>
2023-11-13 03:12:56 +0700

    fix no auth

--- web/src/service/axiosInstance.ts
+++ web/src/service/axiosInstance.ts
@@ -1,21 +1,20 @@
 import axios from 'axios'
 
-function getAxiosInstance(path: string) {
-    const token: string | undefined = localStorage.getItem('token')
-    if (!path) {
-        throw new Error('ใส่ path ด้วย')
-    }
-    if (!token) {
-        throw new Error('ใส่ token ด้วย')
-    }
-    const axiosInstance = axios.create({
-        baseURL: path,
-        headers: {
-            Authorization: 'Bearer + '
-        }
-    })
-    return axiosInstance
+function getAxiosInstance(path: string, withToken: boolean = true) {
+  const token: string | null = localStorage.getItem('token')
+  if (!path) {
+    throw new Error('ใส่ path ด้วย')
+  }
+  if (!token && withToken) {
+    throw new Error('ใส่ token ด้วย')
+  }
+  const axiosInstance = axios.create({
+    baseURL: path,
+    headers: {
+      Authorization: token ? 'Bearer + ' + token : undefined,
+    },
+  })
+  return axiosInstance
 }
-
 
 export default getAxiosInstance
--- web/src/service/myAuthService.ts
+++ web/src/service/myAuthService.ts
@@ -1,16 +1,16 @@
-import getAxiosInstance from "./axiosInstance";
+import getAxiosInstance from './axiosInstance'
 
 export type LoginRequestModel = {
-    username: string;
-    password: string;
+  username: string
+  password: string
 }
 export type LoginResponse = {
-    accessToken: string;
-    refreshToken: string;
+  accessToken: string
+  refreshToken: string
 }
 
-const axios = getAxiosInstance('http://localhost:8080/auth-service')
+const axios = getAxiosInstance('http://localhost:8080/auth-service', false)
 
 export async function login(body: LoginRequestModel) {
-    return axios.post<LoginResponse>('/auth/login', body)
+  return axios.post<LoginResponse>('/auth/login', body)
 }
